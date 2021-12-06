b98090d817bbef3c1bc4c5d7859f50e6f54a7d72
Caroline Galecki <carolinegalecki@gmail.com>
2021-12-06 00:05:22 -0500

    scenario 2 complete for day 2

--- day_2_dive/dive.py
+++ day_2_dive/dive.py
@@ -20,8 +20,31 @@ class Dive(object):
         multiplied = depth * horizontal_pos
         print(f'Scenario 1 output is {multiplied}')
 
+    @staticmethod
+    def second_scenario(self):
+        depth = 0
+        horizontal_pos = 0
+        aim = 0
+
+        # convert each item to int type
+        for index in range(0, len(input_list_temp)):
+            current_direction, current_direction_count_str = input_list_temp[index].split()
+            current_direction_count = int(current_direction_count_str)
+
+            if current_direction == 'forward':
+                horizontal_pos += current_direction_count
+                depth += aim * current_direction_count
+            elif current_direction == 'down':
+                aim += current_direction_count
+            elif current_direction == 'up':
+                aim -= current_direction_count
+
+        multiplied = depth * horizontal_pos
+        print(f'Scenario 2 output is {multiplied}')
+
 
 if __name__ == '__main__':
     input_string = input('Enter coordinates separated by a comma. NOTE for a long list of numbers, put them in a file and < it into the script with "python3 dive.py < file.txt".: \n')
     input_list_temp = input_string.split(',')
     Dive().first_scenario(input_list_temp)
+    Dive().second_scenario(input_list_temp)
