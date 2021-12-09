6045f7b97ba09fa7442096d8ff28d069a61f7fda
Caroline Galecki <carolinegalecki@gmail.com>
2021-12-08 23:36:54 -0500

    Completed day 3

--- day_3_binary_diagnostic/binary_diagnostic.py
+++ day_3_binary_diagnostic/binary_diagnostic.py
@@ -30,26 +30,60 @@ def first_scenario(self):
 
 def second_scenario(self):
     zero_count_array, one_count_array = get_count_arrays_from_list(input_list)
-    numbers_to_consider = []
+    # numbers to consider set here just for initial if.
+    numbers_to_consider = input_list
+    bit_count = str(input_list[0])
+    final_oxygen = ''
+    final_co2 = ''
 
-    # TODO make this a recursey boi
+    for index, character in enumerate(bit_count):
+        # for oxygen rating, get higher first number count from the count arrays. Higher number goes to oxygen.
 
-    # for oxygen rating, get higher first number count from the count arrays. Higher number goes to oxygen.
-    # Lower number goes to CO2 scrubber rating
-    if one_count_array.get(0) >= zero_count_array.get(0):
-        # go with 1 count array
-        numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(one_count_array, 1, 0)
-    else:
-        # go with 0 count array
-        numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(zero_count_array, 0, 0)
+        if len(numbers_to_consider) > 1:
+            if one_count_array.get(index) >= zero_count_array.get(index):
+                # more 1s in the bit count
+                numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(numbers_to_consider, 1, index)
+            else:
+                # more zeros
+                numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(numbers_to_consider, 0, index)
+
+            zero_count_array, one_count_array = get_count_arrays_from_list(numbers_to_consider)
+
+    final_oxygen = numbers_to_consider[0]
+
+    # just set it to one of them at first
+    zero_count_array, one_count_array = get_count_arrays_from_list(input_list)
+    # numbers to consider set here just for initial if.
+    numbers_to_consider = input_list
+
+    for index, character in enumerate(bit_count):
+        # for co2 rating, get lower first number count from the count arrays.
+        if len(numbers_to_consider) > 1:
+            if zero_count_array.get(index) <= one_count_array.get(index):
+                # more 1s in the bit count
+                numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(numbers_to_consider, 0, index)
+            else:
+                # more zeros
+                numbers_to_consider = get_numbers_with_starting_num_from_list_at_index(numbers_to_consider, 1, index)
+            zero_count_array, one_count_array = get_count_arrays_from_list(numbers_to_consider)
+    final_co2 = numbers_to_consider[0]
+
+    oxygen_decimal = int(final_oxygen, 2)
+    co2_decimal = int(final_co2, 2)
+    life_support_rating = oxygen_decimal * co2_decimal
+    print(f'Life support rating: {life_support_rating}')
 
 
 def get_numbers_with_starting_num_from_list_at_index(list_to_search, num_to_match, index_to_observe):
     match_list = []
 
-    for index in range(0, len(list_to_search)):
-        if list_to_search[index][index_to_observe] == num_to_match:
-            match_list.append(list_to_search[index])
+    for item in list_to_search:
+        for char_index, character in enumerate(item):
+            if char_index == index_to_observe:
+                if int(item[index_to_observe]) == int(num_to_match):
+                    match_list.append(item)
+            elif char_index > index_to_observe:
+                continue
 
     return match_list
 
